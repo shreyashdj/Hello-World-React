926b67b5b4a779a1c64ac53dc69b46005978c5e7
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-18 17:22:04 +0530

    [Shreyash]: Added code to display logo image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 //import UC1 from "./components/UC1";
 import UC2 from "./components/UC2";
+import UC3 from "./components/UC3";
 
 function App() {
   return (
     <div className="App">
       <UC2/>
+      <UC3/>
     </div>
   );
 
