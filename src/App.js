c395d5fdd1d5df30f86cddc4d4d30d9296d39d27
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-18 16:57:08 +0530

    [Shreyash]: Added code to display hello message using one way data binding

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-import UC1 from "./components/UC1";
+//import UC1 from "./components/UC1";
+import UC2 from "./components/UC2";
 
 function App() {
   return (
     <div className="App">
-      <UC1/>
+      <UC2/>
     </div>
   );
-  
+
 }
 export default App;
