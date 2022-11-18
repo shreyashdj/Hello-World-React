7a45d5d97448741033b1cce9045debda99d6d49b
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-18 17:21:23 +0530

    [Shreyash]: Added code to add class component which display logo image

---
+++ src/components/UC3.js
@@ -0,0 +1,10 @@
+import logo from './assets/logo.jpg'
+
+function Logo() {
+    return (
+        <img src={logo}
+            alt="A Bridgelabz logo: a Bridge to Employee through lab work"></img >
+    )
+}
+
+export default Logo;
