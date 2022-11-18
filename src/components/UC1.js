31fb69fd28d5905416d47ebb8a9257fb7efcf095
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-18 16:01:48 +0530

    [Shreyash]: Added code to add class component which display hello message

---
+++ src/components/UC1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class HelloMsg extends React.Component {
+    render() {
+        return (
+            <div className="HelloMsg">
+                <h1>
+                    Hello From Bridgelabz
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default HelloMsg;
